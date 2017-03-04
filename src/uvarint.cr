f400eab39e982a3da37f280312accc6a41452558
Brandon Freitag <freitagbr@gmail.com>
2017-03-03 21:31:42 -0800

    Implement UVarInt as a struct
    Implement UVarInt as a struct, and provide several ways to instantiate:
    from an Unsigned Integer, from Bytes, or from Enumerable(UInt8).
    Provide accessors for the UVarInt both encoded and decoded.

--- src/uvarint.cr
+++ src/uvarint.cr
@@ -6,39 +6,66 @@ require "./uvarint/*"
 MSB = 0x80_u8  # 10000000
 REST = 0x7F_u8 # 01111111
 
-module UVarint
-  extend self
+struct UVarInt
+  @encoded : Bytes
+  @decoded : UInt64
 
-  alias UVarInt = Array(UInt8)
+  def initialize(int : Int::Unsigned)
+    @decoded = int.to_u64
+    @encoded = encode @decoded
+  end
+
+  def initialize(bytes : Bytes)
+    @encoded = bytes
+    @decoded = decode bytes
+  end
+
+  def initialize(en : Enumerable(UInt8))
+    raise Exception.new "overflow" if en.size >= 10
+    bytes = Bytes.new(en.size) { |i| en[i] }
+    @encoded = bytes
+    @decoded = decode bytes
+  end
+
+  # Accessors
+  def encoded
+    @encoded
+  end
+
+  def decoded
+    @decoded
+  end
 
   # encode encodes a UInt64 into a UVarInt,
   # which is an Array of UInt8's.
-  def encode(n : UInt64) : UVarInt
-    buf = [] of UInt8
+  private def encode(n : UInt64) : Bytes
+    ptr = Pointer(UInt8).malloc 10
+    i = 0
     while n >= MSB
-      buf << (n.to_u8 | MSB)
+      ptr[i] = n.to_u8 | MSB
+      i += 1
       n = n >> 7
     end
-    buf << n.to_u8
-    return buf
+    ptr[i] = n.to_u8
+    Bytes.new(ptr, i + 1)
   end
 
-  # decode decodes a UVarInt into a UInt64.
-  # If the UVarInt contains more than 10 UInt8's,
+  # decode decodes enumerable bytes into a UInt64.
+  # If the enumeration is longer thar 10 bytes,
   # then an overflow exception is raised.
-  def decode(vi : UVarInt) : UInt64
+  private def decode(bytes : Bytes) : UInt64
     x = 0_u64
     s = 0
-    vi.each_with_index do |v, i|
-      if v < MSB
-        if i > 9 || i == 9 && v > 1
+    bytes.each_with_index do |b, i|
+      if b < MSB
+        if i > 9 || i == 9 && b > 1
           raise Exception.new("overflow")
         end
-        return x | (v.to_u64 << s)
+        return x | (b.to_u64 << s)
       end
-      x |= (v & REST).to_u64 << s
+      x |= (b & REST).to_u64 << s
       s += 7
     end
-    return 0_u64
+    0_u64
   end
 end

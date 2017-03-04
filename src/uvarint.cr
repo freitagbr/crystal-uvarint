04b044b901da489f0ddb96ed6beac362388de41a
Brandon Freitag <freitagbr@gmail.com>
2017-03-04 01:48:40 -0800

    Move private methods up in scope

--- src/uvarint.cr
+++ src/uvarint.cr
@@ -3,6 +3,38 @@ require "./uvarint/*"
 # This module is based on the varint implementation in the Go programming
 # language. See: https://golang.org/src/encoding/binary/varint.go
 
+# Encodes an Int::Unsined into a Bytes slice.
+private def encode(n : Int::Unsigned) : Bytes
+  ptr = Pointer(UInt8).malloc 10
+  i = 0
+  while n >= 0x80_u8
+    ptr[i] = n.to_u8 | 0x80_u8
+    i += 1
+    n = n >> 7
+  end
+  ptr[i] = n.to_u8
+  Bytes.new(ptr, i + 1)
+end
+
+# Decodes enumerable bytes into a UInt64.
+# If the enumeration is longer thar 10 bytes,
+# then an overflow exception is raised.
+private def decode(bytes : Bytes) : UInt64
+  x = 0_u64
+  s = 0
+  bytes.each_with_index do |b, i|
+    if b < 0x80_u8
+      if i > 9 || i == 9 && b > 1
+        raise Exception.new "overflow"
+      end
+      return x | (b.to_u64 << s)
+    end
+    x |= (b & 0x7F_u8).to_u64 << s
+    s += 7
+  end
+  0_u64
+end
+
 struct UVarInt
   @encoded : Bytes
   @decoded : UInt64
@@ -41,37 +73,5 @@ struct UVarInt
 
   def decoded
     @decoded
-  end
-
-  # Encodes an Int::Unsined into a Bytes slice.
-  private def encode(n : Int::Unsigned) : Bytes
-    ptr = Pointer(UInt8).malloc 10
-    i = 0
-    while n >= 0x80_u8
-      ptr[i] = n.to_u8 | 0x80_u8
-      i += 1
-      n = n >> 7
-    end
-    ptr[i] = n.to_u8
-    Bytes.new(ptr, i + 1)
-  end
-
-  # Decodes enumerable bytes into a UInt64.
-  # If the enumeration is longer thar 10 bytes,
-  # then an overflow exception is raised.
-  private def decode(bytes : Bytes) : UInt64
-    x = 0_u64
-    s = 0
-    bytes.each_with_index do |b, i|
-      if b < 0x80_u8
-        if i > 9 || i == 9 && b > 1
-          raise Exception.new "overflow"
-        end
-        return x | (b.to_u64 << s)
-      end
-      x |= (b & 0x7F_u8).to_u64 << s
-      s += 7
-    end
-    0_u64
   end
 end
